85251969506f65693bac4612773863002c74e4b3
ilkol <ilkolreserv@gmail.com>
2025-10-05 22:42:56 +0400

    Создал класс для основной логики

---
+++ crypto.cpp
@@ -0,0 +1,13 @@
+#include "crypto.h"
+
+QString Crypto::encrypt(const QString& message) {
+    return message;
+}
+
+QString Crypto::decrypt(const QString& message) {
+    return message;
+}
+
+QString Crypto::hack(const QString& message) {
+    return message;
+}
---
+++ crypto.h
@@ -0,0 +1,13 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+class Crypto
+{
+public:
+    Crypto() = delete;
+    static QString encrypt(const QString& message);
+    static QString decrypt(const QString& message);
+    static QString hack(const QString& message);
+};
+
+#endif // CRYPTO_H
