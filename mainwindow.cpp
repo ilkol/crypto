9a9181c991654489dda0d0e361e53f420a47110d
ilkol <ilkolreserv@gmail.com>
2025-10-05 22:51:57 +0400

    Навесил сигналы на кнопки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "./crypto.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,26 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::processClickButton(std::function<QString(const QString&)> func) {
+    QString input = ui->textEditMessage->toPlainText();
+    ui->textEditResult->setText(func(input));
+}
+
+void MainWindow::on_pushButtonEncrypt_clicked()
+{
+    processClickButton(Crypto::encrypt);
+}
+
+
+void MainWindow::on_pushButtonDecipher_clicked()
+{
+    processClickButton(Crypto::decrypt);
+}
+
+
+void MainWindow::on_pushButtonHack_clicked()
+{
+    processClickButton(Crypto::hack);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <functional>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void processClickButton(std::function<QString(const QString&)> func);
+
+private slots:
+    void on_pushButtonEncrypt_clicked();
+    void on_pushButtonDecipher_clicked();
+    void on_pushButtonHack_clicked();
 };
 #endif // MAINWINDOW_H
